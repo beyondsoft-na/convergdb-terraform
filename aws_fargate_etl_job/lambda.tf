6086217598438cab999b5a8eb5246eb789338927
Jeremy Winters <jeremyranierwinters@gmail.com>
2018-05-22 15:37:41 -0700

    fix iam policy

--- aws_fargate_etl_job/lambda.tf
+++ aws_fargate_etl_job/lambda.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role_policy" "run_task" {
   "Statement": [
     {
       "Action": [
-        "glue:RunTask"
+        "ecs:RunTask"
       ],
       "Effect": "Allow",
       "Resource": "*"
