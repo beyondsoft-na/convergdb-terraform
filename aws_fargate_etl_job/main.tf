e38d67c3b958fac42f4cf176517c1f4323f8a0e5
Zuqing Li <zuqingl@gmail.com>
2019-04-08 12:30:08 -0500

    use cloudwatch event to trigger fargate directly

--- aws_fargate_etl_job/main.tf
+++ aws_fargate_etl_job/main.tf
@@ -17,10 +17,10 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket_object" "convergdb_library" {
-  bucket   = "${var.script_bucket}"
-  key      = "${var.pyspark_library_key}"
-  source   = "${var.local_pyspark_library}"
-  etag     = "${md5(file("${var.local_pyspark_library}"))}"
+  bucket = "${var.script_bucket}"
+  key    = "${var.pyspark_library_key}"
+  source = "${var.local_pyspark_library}"
+  etag   = "${md5(file("${var.local_pyspark_library}"))}"
 
   tags {
     "convergdb:deployment" = "${var.deployment_id}"
@@ -31,20 +31,20 @@ data "template_file" "script_object_source" {
   template = "${file("${var.local_script}")}"
 
   vars {
-    admin_bucket = "${var.admin_bucket}"
-    data_bucket = "${var.data_bucket}"
-    deployment_id = "${var.deployment_id}"
-    region = "${var.region}"
-    sns_topic = "${var.sns_topic}"
+    admin_bucket         = "${var.admin_bucket}"
+    data_bucket          = "${var.data_bucket}"
+    deployment_id        = "${var.deployment_id}"
+    region               = "${var.region}"
+    sns_topic            = "${var.sns_topic}"
     cloudwatch_namespace = "${var.cloudwatch_namespace}"
   }
 }
 
 resource "aws_s3_bucket_object" "script_object" {
-  bucket   = "${var.script_bucket}"
-  key      = "${var.script_key}"
-  content  = "${data.template_file.script_object_source.rendered}"
-  etag     = "${md5("${data.template_file.script_object_source.rendered}")}"
+  bucket  = "${var.script_bucket}"
+  key     = "${var.script_key}"
+  content = "${data.template_file.script_object_source.rendered}"
+  etag    = "${md5("${data.template_file.script_object_source.rendered}")}"
 
   tags {
     "convergdb:deployment" = "${var.deployment_id}"
@@ -52,14 +52,15 @@ resource "aws_s3_bucket_object" "script_object" {
 }
 
 resource "aws_iam_role" "ecs_task_role" {
-  name = "convergdb-${var.deployment_id}-${var.etl_job_name}-task-role"
-  path = "/"
+  name               = "convergdb-${var.deployment_id}-${var.etl_job_name}-task-role"
+  path               = "/"
   assume_role_policy = "${data.aws_iam_policy_document.ecs_assume_role_policy.json}"
 }
 
 data "aws_iam_policy_document" "ecs_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
+
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
@@ -70,6 +71,7 @@ data "aws_iam_policy_document" "ecs_assume_role_policy" {
 resource "aws_iam_role_policy" "s3_access" {
   name = "convergdb-${var.deployment_id}-${var.etl_job_name}-access-policy"
   role = "${aws_iam_role.ecs_task_role.name}"
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -151,13 +153,14 @@ EOF
 }
 
 resource "aws_ecs_task_definition" "convergdb_ecs_task" {
-  family = "convergdb-${var.deployment_id}-${var.etl_job_name}"
-  network_mode = "awsvpc"
+  family                   = "convergdb-${var.deployment_id}-${var.etl_job_name}"
+  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu = "${var.fargate_cpu}"
-  memory = "${var.fargate_memory}"
-  task_role_arn = "${aws_iam_role.ecs_task_role.arn}"
-  execution_role_arn = "${var.execution_task_role}"
+  cpu                      = "${var.fargate_cpu}"
+  memory                   = "${var.fargate_memory}"
+  task_role_arn            = "${aws_iam_role.ecs_task_role.arn}"
+  execution_role_arn       = "${var.execution_task_role}"
+
   container_definitions = <<DEFINITION
 [
   {
@@ -198,20 +201,67 @@ resource "aws_ecs_task_definition" "convergdb_ecs_task" {
 DEFINITION
 }
 
+resource "aws_iam_role" "ecs_events" {
+  name = "cdb-${var.deployment_id}-${var.etl_job_name}-event-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_events_policy" {
+  role = "${aws_iam_role.ecs_events.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": [
+            "ecs:*",
+            "iam:PassRole"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_cloudwatch_event_rule" "convergdb_etl" {
-  name        = "convergdb-${var.deployment_id}-${var.etl_job_name}-trigger"
-  description = "convergdb etl job ${var.etl_job_name}"
+  name                = "convergdb-${var.deployment_id}-${var.etl_job_name}-trigger"
+  description         = "convergdb etl job ${var.etl_job_name}"
   schedule_expression = "${var.etl_job_schedule}"
-  is_enabled = false
+  is_enabled          = true
 }
 
 resource "aws_cloudwatch_event_target" "ecs_task" {
   rule      = "${aws_cloudwatch_event_rule.convergdb_etl.name}"
   target_id = "convergdb-${var.deployment_id}-${var.etl_job_name}-target"
-  arn = "${var.ecs_cluster}"
-  role_arn = "${aws_iam_role.ecs_task_role.arn}"
+  arn       = "${var.ecs_cluster}"
+  role_arn  = "${aws_iam_role.ecs_events.arn}"
+
   ecs_target {
     task_definition_arn = "${aws_ecs_task_definition.convergdb_ecs_task.arn}"
-    task_count = 1
+    task_count          = 1
+    launch_type         = "FARGATE"
+    platform_version    = "LATEST"
+
+    network_configuration {
+      subnets = ["${var.ecs_subnet}"]
+    }
   }
 }
